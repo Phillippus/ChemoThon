83958cd6bc419f819b2ca11036c26179ebbf52ba
Phillippus <filip.kohutek@gmail.com>
2024-08-15 20:49:50 +0200

    Major fixes

--- ChemoThonBrs.py
+++ ChemoThonBrs.py
@@ -2,7 +2,7 @@ import streamlit as st
 import json
 
 def load_json(filename):
-    """ Loads JSON data from a specified file with error handling. """
+    """Loads JSON data from a specified file with error handling."""
     try:
         with open(f'data/{filename}', 'r') as file:
             return json.load(file)
@@ -13,30 +13,32 @@ def load_json(filename):
         st.error("Chyba pri dekódovaní JSON. Skontrolujte formát súboru.")
         return None
 
-def display_chemotherapy_details(rbodysurf, filename):
-    """ Displays detailed information about the chemotherapy regimen using body surface area. """
-    chemo_json = load_json(filename)
-    if chemo_json:
-        regimen_name = filename.replace('.json', '')
-        st.write(f"### Protokol {regimen_name}")
-        for chemo in chemo_json['Chemo']:
-            dosage = round(chemo['Dosage'] * rbodysurf, 2)
-            st.write(f"{chemo['Name']} {chemo['Dosage']} mg/m² ......... {dosage} mg D {chemo['Day']}")
+def display_chemotherapy_details(rbodysurf, chemoType):
+    """Displays detailed information about the chemotherapy regimen using body surface area."""
+    chemoJson = load_json(chemoType)
+    if chemoJson:
+        st.write(f"### Protokol {chemoType.replace('.json', '')}")
+        for chemo in chemoJson["Chemo"]:
+            dosage = round(chemo["Dosage"] * rbodysurf, 2)
+            st.write(f"{chemo['Name']} {round(chemo['Dosage'], 2)} {chemo['DosageMetric']} ......... {dosage} mg D {chemo['Day']}")
 
-        st.write(f"                       NC {chemo_json.get('NC', 'Nie je určené')} . deň")
-        st.write(" ")
+        st.write(f"""         
+                       NC {chemoJson["NC"]} . deň
+                                            """)
+    
         st.write("                                     D1")
-        st.write(chemo_json['Day1']['Premed']['Note'])
-        for instruction in chemo_json['Day1']['Instructions']:
-            drug_name = instruction['Name']
-            dosage = next((item['Dosage'] for item in chemo_json['Chemo'] if item['Name'] == drug_name), None)
-            if dosage:
-                adjusted_dosage = round(dosage * rbodysurf, 2)
-                st.write(f"{drug_name} {adjusted_dosage} mg {instruction['Inst']}")
+        st.write(chemoJson["Day1"]["Premed"]["Note"])
+    
+        for i in range(len(chemoJson["Chemo"])):
+            drug_name = chemoJson["Day1"]["Instructions"][i]["Name"]
+            dosage = round(chemoJson["Chemo"][i]["Dosage"] * rbodysurf, 2)
+            instruction = chemoJson["Day1"]["Instructions"][i]["Inst"]
+            st.write(f"{drug_name} {dosage} mg {instruction}")
 
 def calculate_bsa(weight, height):
-    """ Calculates body surface area using the DuBois formula. """
-    return round((weight**0.425) * (height**0.725) * 0.007184, 2)
+    """Calculates body surface area using the DuBois formula."""
+    bodysurf = (weight**0.425) * (height**0.725) * 0.007184
+    return round(bodysurf, 2)
 
 def main():
     st.title("ChemoThon - BreastSK v. 2.0")
@@ -47,27 +49,29 @@ def main():
     Autor nezodpovedá za prípadné škody spôsobené jeho použitím!
     Pripomienky a požiadavky na úpravu posielajte na filip.kohutek@fntn.sk""")
 
-    weight = st.number_input("Zadajte hmotnosť (kg):", min_value=1, max_value=250, step=1)
-    height = st.number_input("Zadajte výšku (cm):", min_value=1, max_value=250, step=1)
+    weight = st.number_input("Zadajte hmotnosť (kg):", min_value=1, max_value=250, step=1, value=None)
+    height = st.number_input("Zadajte výšku (cm):", min_value=1, max_value=250, step=1, value=None)
     
-    if st.button("Vypočítať BSA"):
+    if st.button("Vypočítať BSA") and weight is not None and height is not None:
         rbodysurf = calculate_bsa(weight, height)
         st.session_state['rbodysurf'] = rbodysurf
-        st.write(f"Vypočítaný telesný povrch (BSA): {rbodysurf} m²")
+        st.write(f"Telesný povrch je: {rbodysurf} m²")
 
     if 'rbodysurf' in st.session_state:
+        st.write(f"Telesný povrch (BSA): {st.session_state['rbodysurf']} m²")
+
         chemo_options = {
             "EC": "EC.json",
             "AC": "AC.json",
-            "Dose-dense AC": "dd-AC.json",
-            "Docetaxel": "docetaxelbreast.json",
-            "Weekly Paclitaxel": "paclitaxelweekly.json",
-            "Capecitabine": "capecitabine.json",
-            "Gemcitabine": "gemcitabine.json",
-            "Weekly Vinorelbine": "vinorelbinweekly.json",
-            "Eribulin": "eribulin.json",
-            "Pegylated Liposomal Doxorubicin": "pegdoxo.json",
-            "TDM-1": "TDM1.json"
+            "dd-AC + G-CSF": "dd-AC.json",
+            "docetaxel + G-CSF": "docetaxelbreast.json",
+            "paclitaxel": "paclitaxelweekly.json",
+            "kapecitabin": "capecitabine.json",
+            "gemcitabin": "gemcitabine.json",
+            "vinorelbin p.o. weekly": "vinorelbinweekly.json",
+            "eribulin": "eribulin.json",
+            "peg- doxorubicin": "pegdoxo.json",
+            "TD-M1": "TDM1.json"
         }
 
         chemo_name = st.selectbox("Vyberte chemoterapeutický režim:", list(chemo_options.keys()))
