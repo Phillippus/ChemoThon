4068c13b1b8b9e91675659cc2182ecf9cbb62cf2
Phillippus <filip.kohutek@gmail.com>
2025-01-19 14:34:19 +0100

    inscription fix

--- ChemoThonBrsEng.py
+++ ChemoThonBrsEng.py
@@ -65,7 +65,11 @@ def display_chemotherapy_details(protocol, bsa, weight):
 
 def main():
     st.title("ChemoThon Breast v. 3.0 ENG")
-    st.write("This program prescribes chemotherapy regimens based on body surface area (BSA) or weight.")
+    st.write("""Welcome to ChemoThon!
+This application provides assistance in prescribing chemotherapy regimens based on body surface area (BSA) or weight,
+Please ensure that doses are adjusted to align with the packaging and protocols available in your country. Users bear full responsibility for applying this tool in clinical practice.
+
+We welcome your feedback to improve this app further. Feel free to reach out at filip.kohutek@fntn.sk.""")
 
     # Load chemotherapy data
     data = load_chemotherapy_data()
